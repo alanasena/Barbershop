a97daa46e256452afbf593278801e350578e1fc3
Alana Sena <alanaoliveira2004@alu.ufc.br>
2026-01-06 09:29:29 -0300

    resolvendo mais coisas

--- client/src/components/Admin/AppointmentsList/TableData/TableData.js
+++ client/src/components/Admin/AppointmentsList/TableData/TableData.js
@@ -1,28 +1,39 @@
-import React from 'react'
-import './TableData.css'
+import React from 'react';
+import PropTypes from 'prop-types';
+import './TableData.css';
 
-const TableData = ({appointments}) => {
+const TableData = ({ appointments }) => {
+  return (
+    <td>
+      {appointments[0] ? (
+        <span className="td-color">
+          <span className="td-flex-icons">
+            <i className="fa fa-user" aria-hidden="true"></i>
+            {appointments[0].name.charAt(0).toUpperCase() +
+              appointments[0].name.slice(1)}
+          </span>
 
-    return (
-        <td>
-            {
-            appointments[0] ?
-             <span className='td-color'>
-                <span className='td-flex-icons'>
-                    <i className="fa fa-user" aria-hidden="true"></i>
-                    {appointments[0].name.charAt(0).toUpperCase() + appointments[0].name.slice(1)}
-                </span>
-              
-                <span className='td-flex-icons'>
-                    <i className="fa fa-phone" aria-hidden="true"></i>
-                    <a href={`tel:${appointments[0].phone}`}>{appointments[0].phone}</a>            
-                </span>    
-            </span> 
-            
-            : <span className='td-empty'>Empty</span>
-            } 
-        </td>
-    )
-}
+          <span className="td-flex-icons">
+            <i className="fa fa-phone" aria-hidden="true"></i>
+            <a href={`tel:${appointments[0].phone}`}>
+              {appointments[0].phone}
+            </a>
+          </span>
+        </span>
+      ) : (
+        <span className="td-empty">Empty</span>
+      )}
+    </td>
+  );
+};
 
-export default TableData
+TableData.propTypes = {
+  appointments: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      phone: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+export default TableData;
--- client/src/components/Admin/UsersList/UserRow/UserRow.js
+++ client/src/components/Admin/UsersList/UserRow/UserRow.js
@@ -1,25 +1,37 @@
-import React from 'react'
-import './UserRow.css'
+import React from 'react';
+import PropTypes from 'prop-types';
+import './UserRow.css';
 
-const UserRow = ({user, deleteUser}) => {
-    console.log('user row rendred')
+const UserRow = ({ user, deleteUser }) => {
+  console.log('user row rendered');
 
-    return (
+  return (
+    <tr className="table-row">
+      <td>
+        {user.name.charAt(0).toUpperCase() + user.name.slice(1)}
+      </td>
+      <td>{user.email}</td>
+      <td>{user.phone ? user.phone : 'Empty'}</td>
+      <td className="bg-action-white">
+        <button
+          onClick={() => deleteUser(user._id)}
+          className="btn-icon btn-red-color"
+        >
+          <i className="fa fa-trash" aria-hidden="true"></i>
+        </button>
+      </td>
+    </tr>
+  );
+};
 
-        <tr className='table-row'>
-            <td>{user.name.charAt(0).toUpperCase() + user.name.slice(1)}</td>
-            <td>{user.email}</td>
-            <td>{user.phone? user.phone :'Empty'}</td>
-            <td className='bg-action-white'>
-                {/* <button onClick={() =>editUser(user._id)} className='btn-icon btn-blue-color'>
-                    <i className="fa fa-pencil" aria-hidden="true"></i>
-                </button> */}
-                <button onClick={() => deleteUser(user._id)} className='btn-icon btn-red-color'>
-                    <i className="fa fa-trash" aria-hidden="true"></i>
-                </button>
-            </td>  
-        </tr>           
-    )
-}
+UserRow.propTypes = {
+  user: PropTypes.shape({
+    _id: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    phone: PropTypes.string,
+  }).isRequired,
+  deleteUser: PropTypes.func.isRequired,
+};
 
-export default UserRow
+export default UserRow;
