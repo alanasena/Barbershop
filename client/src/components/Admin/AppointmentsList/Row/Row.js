8d82a55991b754d98912a4a794d6eb8fea131317
Alana Sena <alanaoliveira2004@alu.ufc.br>
2026-01-06 08:58:41 -0300

    resolvendo umas coisas

--- client/src/components/Admin/AppointmentsList/Row/Row.js
+++ client/src/components/Admin/AppointmentsList/Row/Row.js
@@ -1,27 +1,31 @@
-import React from 'react'
-import './Row.css'
-import TableData from '../TableData/TableData'
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Row.css';
+import TableData from '../TableData/TableData';
 
-const Row = ({appointments, time}) => {
-    console.log('Row rendred')
- 
-    return (
-  
-        <tr>
-            <th className='left-th'>{time}</th>
-            <TableData appointments={appointments.filter(obj => obj.day === 'Sun')} />
-            <TableData appointments={appointments.filter(obj => obj.day === 'Mon')} />
-            <TableData appointments={appointments.filter(obj => obj.day === 'Tue')} />
-            <TableData appointments={appointments.filter(obj => obj.day === 'Wed')} />
-            <TableData appointments={appointments.filter(obj => obj.day === 'Thu')} />
-            <TableData appointments={appointments.filter(obj => obj.day === 'Fri')} />
-        </tr>
-    )
-}
+const Row = ({ appointments, time }) => {
+  console.log('Row rendered');
 
-export default Row
+  return (
+    <tr>
+      <th className="left-th">{time}</th>
+      <TableData appointments={appointments.filter(obj => obj.day === 'Sun')} />
+      <TableData appointments={appointments.filter(obj => obj.day === 'Mon')} />
+      <TableData appointments={appointments.filter(obj => obj.day === 'Tue')} />
+      <TableData appointments={appointments.filter(obj => obj.day === 'Wed')} />
+      <TableData appointments={appointments.filter(obj => obj.day === 'Thu')} />
+      <TableData appointments={appointments.filter(obj => obj.day === 'Fri')} />
+    </tr>
+  );
+};
 
+Row.propTypes = {
+  appointments: PropTypes.arrayOf(
+    PropTypes.shape({
+      day: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  time: PropTypes.string.isRequired,
+};
 
-
-
-
+export default Row;
